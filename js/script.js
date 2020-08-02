e656341c9727f335ffa5e5bb97b71005cbfae20a
Vitalii-Hapon <vitali_gapon@gmail.com>
2020-08-02 13:28:03 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
-   $('.slider').slick();
+    $('.slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        appendDots:$('.arrow-wrapper'),
+        appendArrows:$('.arrow-wrapper'),
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+});
+    $('.slick-arrow').blur();
 
 });
